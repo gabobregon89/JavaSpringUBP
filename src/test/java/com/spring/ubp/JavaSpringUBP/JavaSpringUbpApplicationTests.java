dad438cbf2a4fecafff3aa65ade95bd5c002a80e
gabobregon89 <gab.obregon89@gmail.com>
2023-08-13 12:41:33 -0300

    project's structure

---
+++ src/test/java/com/spring/ubp/JavaSpringUBP/JavaSpringUbpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.ubp.JavaSpringUBP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringUbpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
